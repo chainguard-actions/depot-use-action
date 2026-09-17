1a05e436a2fa7d1fbae925ceb7e9180c8868b79a
actionchain <actionchain@chainguard.dev>
2026-09-17 00:20:48 +0000

    Update depot/use-action@v1.3.0

---
+++ build.cjs
@@ -0,0 +1,10 @@
+const esbuild = require('esbuild')
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  target: 'node20',
+  outfile: 'dist/index.js',
+})
